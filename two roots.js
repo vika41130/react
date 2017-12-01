7f155ca708fcd4ae467484d641bdf67bbdaf1979
vika41130 <vika41130@gmail.com>
2017-12-01 17:13:46 +0700

    Add files via upload

---
+++ two roots.js
@@ -0,0 +1,16 @@
+// root1
+ReactDOM.render(
+<div>
+  <h1 className="yellow">frontend - root1</h1>
+</div>
+, document.getElementById('root')
+);
+
+// root2
+ReactDOM.render(
+<div>
+  <h1 className="yellow">frontend - root2</h1>
+  <h1 className="yellow">frontend - root2</h1>
+</div>
+, document.getElementById('root2')
+);
