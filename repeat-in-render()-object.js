2b3183f1f2beb6e58590f5afaa30bc762e56cd21
vika41130 <vika41130@gmail.com>
2017-12-08 17:00:57 +0700

    Add files via upload

---
+++ repeat-in-render()-object.js
@@ -0,0 +1,49 @@
+class Img extends React.Component {
+  constructor(props) {
+    super(props);
+    this.addImg = this.addImg.bind(this);
+    this.state = {
+      imgArray: [
+        {src: "img/img1.jpg", description: "img1"}, {src: "img/img2.jpg", description: "img2"},
+        {src: "img/img3.jpg", description: "img3"}, {src: "img/img4.jpg", description: "img4"}
+      ],
+      currentId: 0
+    };
+  }
+
+  addImg = () => {
+    this.state.imgArray.unshift({src: "img/img5.png", description: "img5"});
+    this.setState({});
+  }
+
+  componentDidMount() {
+
+  }
+
+  componentWillUnmount() {
+
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.addImg}>Add New Img</button>
+        {
+          this.state.imgArray.map((img, index)=>{
+            return(
+              <div>
+                <img src={img.src} key={index} />
+                <p key={index}>{img.description}</p>
+              </div>
+            );
+          })
+        }
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Img />,
+  document.getElementById('root')
+);
