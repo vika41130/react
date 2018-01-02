bea1d6331cb7d77a270261f6469fa9b6681d3d6d
vika41130 <vika41130@gmail.com>
2018-01-02 09:28:17 +0700

    Update login-logout-index.js

--- login-logout-index.js
+++ login-logout-index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import LoginControl from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <LoginControl />,
