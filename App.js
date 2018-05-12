7ad6432f894d3a09b99cd96ea08ffd910dcd054c
vika41130 <vika41130@gmail.com>
2018-05-12 16:08:59 +0700

    Add files via upload

--- App.js
+++ App.js
@@ -5,16 +5,29 @@ import './App.css';
 // import Product from './components/Product';
 
 class App extends Component {
-  showProductInfo(product) {
-    if(product.status) {
-      return <div style={{fontSize: '150%'}}>
-      <strong>ID</strong>: {product.id}<br />
-      <strong>Name: </strong>{product.name} <br />
-      <strong>Price: </strong>{product.price} <br />
-      <strong>Status: </strong>{product.status ? 'Active' : 'Peding'}
-    </div>
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [
+        {
+          id: 0,
+          username: 'user1',
+          salary: 500
+        },
+        {
+          id: 1,
+          username: 'user2',
+          salary: 800
+        },
+        {
+          id: 2,
+          username: 'user3',
+          salary: 1200
+        }
+      ]
     }
   }
+
   showUsers(users) {
     var element = users.map((value, index) => {
       return <h3 key={value.id}>
@@ -25,43 +38,11 @@ class App extends Component {
     });
     return element;
   }
+
   render() {
-    var product = {
-      id: 1,
-      name: 'Iphone 7 Plus',
-      price: 123.123,
-      status: false
-    };
-    var users = [
-      {
-        id: 0,
-        username: 'user1',
-        salary: 500
-      },
-      {
-        id: 1,
-        username: 'user2',
-        salary: 800
-      },
-      {
-        id: 2,
-        username: 'user3',
-        salary: 1200
-      }
-    ];
     return (
       <div>
-        {/* {this.showProductInfo(product)} */}
-        {this.showUsers(users)}
-        {/* {
-          users.map((value, index) => {
-            return <h3 key={index}>
-              ID: {value.id} <br />
-              Username: {value.username} <br />
-              Salary: {value.salary}
-            </h3>
-          })
-        } */}
+        {this.showUsers(this.state.users)}
       </div>
     );
   }
