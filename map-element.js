e5e8e2050cbc823ef70c3eaef64f7db91936420e
vika41130 <vika41130@gmail.com>
2018-05-12 16:10:43 +0700

    Update and rename App.js to map-element.js

--- map-element.js
+++ map-element.js
@@ -1,8 +1,4 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
-import './App.css';
-// import Header from './components/Header';
-// import Product from './components/Product';
 
 class App extends Component {
   constructor(props) {
